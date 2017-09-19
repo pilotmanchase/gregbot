f3fc4700b0175ebc3bc4b9ef98769fe6c638a378
pilotmanchase <chasefree@comcast.net>
2017-09-18 20:23:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(botRegex);
     this.res.end();
   } else {
     console.log("don't care");
