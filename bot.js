3ae01b01705db86ffc66000d406d536a06444999
pilotmanchase <chasefree@comcast.net>
2017-09-18 21:00:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-  var botRegex = /^\Greg/;///^\/cool guy$/;
+      botRegex = /^\Greg/;///^\/cool guy$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(botRegex);
+    postMessage();
     this.res.end();
   } else {
     console.log("don't care");
