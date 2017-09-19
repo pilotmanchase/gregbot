db7cb5e3528e0d04dbe1506d4a005a61110cd937
pilotmanchase <chasefree@comcast.net>
2017-09-18 21:39:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\Greg/;///^\/cool guy$/;
-      botRegxTest = /^\Nick/;
+  var botRegexTest = /^\Nick/;
   
   var whichOne = 0;
   if(request.text && botRegex.test(request.text)) {
